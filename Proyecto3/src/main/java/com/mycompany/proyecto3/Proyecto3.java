636be85d171375bbb2ec99d56062c9ee334f5318
Hugo <hjcarranzaarroyo@hotmail.com>
2022-06-20 01:13:56 -0600

    Menu

---
+++ Proyecto3/src/main/java/com/mycompany/proyecto3/Proyecto3.java
@@ -0,0 +1,11 @@
+package com.mycompany.proyecto3;
+
+import GUI.Menu;
+
+public class Proyecto3 {
+
+    public static void main(String[] args) {
+        Menu P = new Menu();
+        P.setVisible(true);
+    }
+}
